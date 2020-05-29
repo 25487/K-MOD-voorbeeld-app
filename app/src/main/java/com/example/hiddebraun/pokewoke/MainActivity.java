4537ea887eb293f0b6ac11e88ac92f7984212744
Hidde Braun <hidde.braun@gmail.com>
2020-05-29 13:03:54 +0200

    Start situatie. Scherm en knop + click listener om andere activity te starten

--- app/src/main/java/com/example/hiddebraun/pokewoke/MainActivity.java
+++ app/src/main/java/com/example/hiddebraun/pokewoke/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.hiddebraun.pokewoke;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Gravity;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.snackbar.Snackbar;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -27,25 +21,10 @@ public class MainActivity extends AppCompatActivity {
 
     // Create an anonymous implementation of OnClickListener
     private View.OnClickListener pokeButtonListener = new View.OnClickListener() {
+
         public void onClick(View v) {
-            Snackbar mySnackbar = Snackbar.make(v, R.string.buttonClicked, Snackbar.LENGTH_LONG);
-            mySnackbar.show();
-
-            Toast toast = Toast.makeText(getApplicationContext(), R.string.buttonClicked, Toast.LENGTH_LONG);
-            toast.setGravity(Gravity.CENTER|Gravity.TOP, 0, 200);
-            toast.show();
-
-            LayoutInflater inflater = getLayoutInflater();
-            View layout = inflater.inflate(R.layout.custom_toast,
-                    (ViewGroup) findViewById(R.id.custom_toast_container));
-
-            Toast customToast = new Toast(getApplicationContext());
-            customToast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
-            customToast.setDuration(Toast.LENGTH_LONG);
-            customToast.setView(layout);
-            customToast.show();
-
-
+            Intent pokeBowlIntent = new Intent(v.getContext(), PokeMenuActivity.class);
+            startActivity(pokeBowlIntent);
         }
     };
 
--- app/src/main/java/com/example/hiddebraun/pokewoke/PokeMenuActivity.java
+++ app/src/main/java/com/example/hiddebraun/pokewoke/PokeMenuActivity.java
@@ -1,43 +1,19 @@
 package com.example.hiddebraun.pokewoke;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import android.os.Bundle;
 
-import java.util.ArrayList;
-import java.util.List;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class PokeMenuActivity extends AppCompatActivity {
-
-    private RecyclerView recyclerView;
-    private RecyclerView.Adapter mAdapter;
-    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemenu);
         createPokeMenu();
-
     }
 
     private void createPokeMenu() {
-        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
-        // use this setting to
-        // improve performance if you know that changes
-        // in content do not change the layout size
-        // of the RecyclerView
-        recyclerView.setHasFixedSize(true);
-        // use a linear layout manager
-        layoutManager = new LinearLayoutManager(this);
-        recyclerView.setLayoutManager(layoutManager);
-        List<String> input = new ArrayList<>();
-        for (int i = 0; i < 100; i++) {
-            input.add("Sushi Nagiri " + String.format("%02d", i));
-        }// define an adapter
-        mAdapter = new MyAdapter(input);
-        recyclerView.setAdapter(mAdapter);
+
     }
 }
