0c9c6cad08618aaf66703188a408bae48659ff46
Hidde Braun <hidde.braun@gmail.com>
2020-06-10 21:23:40 +0200

    Loading data from JSON file in assets / local storage in app

--- app/src/main/java/com/example/hiddebraun/pokewoke/PokeMenuActivity.java
+++ app/src/main/java/com/example/hiddebraun/pokewoke/PokeMenuActivity.java
@@ -1,26 +1,16 @@
 package com.example.hiddebraun.pokewoke;
 
-import android.app.ProgressDialog;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.Gravity;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DividerItemDecoration;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.json.JSONTokener;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -39,7 +29,8 @@ public class PokeMenuActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_pokemenu);
-        String url = BuildConfig.POKE_JSON_URL;
+
+        String filename = "pokebowl.json";
 
 
         // Haal de list view (RecyclerView) op
@@ -61,64 +52,38 @@ public class PokeMenuActivity extends AppCompatActivity {
         ingredientListView.addItemDecoration(dividerItemDecoration);
         ingredientListView.setAdapter(ingredientAdapter);
 
-        loadJsonData(url);
+        try {
+            loadJsonData(filename);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void loadJsonData(String url) {
-        final ProgressDialog progressDialog = new ProgressDialog(this);
-        progressDialog.setMessage("Loading Poke Bowl...");
-        progressDialog.setIcon(getDrawable(R.drawable.ic_launcher_foreground));
-        progressDialog.show();
+    /**
+     * @param String filename JSON filename in src/main/assets to load
+     * @throws JSONException
+     */
+    private void loadJsonData(String filename) throws JSONException {
 
-        // Instantiate the RequestQueue.
-        RequestQueue queue = Volley.newRequestQueue(this);
+        String jsonString = Utils.getJsonFromAssets(getApplicationContext(), filename);
+        JSONObject   object = (JSONObject) new JSONTokener(jsonString).nextValue();
+        String baseUrl = object.getString("base_url");
+        JSONArray ingredients = object.getJSONArray("ingredients");
 
-        Response.Listener<JSONObject> responseListener = new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
+        for (int i = 0; i < ingredients.length(); i++) {
+            JSONObject ingredientObj = ingredients.getJSONObject(i);
+            Ingredient ingredient = new Ingredient();
 
-                    Log.d("JSON", response.toString());
+            ingredient.setId(ingredientObj.getInt("id"));
+            ingredient.setName(ingredientObj.getString("name"));
+            ingredient.setPrice(ingredientObj.getDouble("price"));
+            String fullUrl = baseUrl + ingredientObj.getString("image");
+            ingredient.setImageUrl(fullUrl);
 
-                    String baseUrl = response.getString("base_url");
-                    JSONArray ingredients = response.getJSONArray("ingredients");
-                    for (int i = 0; i < ingredients.length(); i++) {
-                        JSONObject ingredientObj = ingredients.getJSONObject(i);
-                        Ingredient ingredient = new Ingredient();
+            ingredientList.add(ingredient);
 
-                        ingredient.setId(ingredientObj.getInt("id"));
-                        ingredient.setName(ingredientObj.getString("name"));
-                        ingredient.setPrice(ingredientObj.getDouble("price"));;
-                        String fullUrl = baseUrl + ingredientObj.getString("image");
-                        ingredient.setImageUrl(fullUrl);
-
-                        ingredientList.add(ingredient);
-
-                    }
-                    ingredientAdapter.notifyDataSetChanged();
-                    progressDialog.dismiss();
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-            }
-        };
-
-        Response.ErrorListener errorListener = new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Toast toast = new Toast(getApplicationContext());
-                toast.setText(error.getMessage());
-                toast.setGravity(Gravity.CENTER, 0, 0);
-                toast.setDuration(2000);
-                toast.show();
-            }
-        };
-
-        // Request a string response from the provided URL.
-        JsonObjectRequest jsonRequest = new JsonObjectRequest(Request.Method.GET, url, null, responseListener, errorListener);
-
-        // Add the request to the RequestQueue.
-        queue.add(jsonRequest);
+        }
+        ingredientAdapter.notifyDataSetChanged();
 
     }
 
